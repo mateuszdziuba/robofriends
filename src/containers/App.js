e731b2290af8d1e24edb1ca60364a3d893e760c8
mateuszdziuba <mkdziuba@gmail.com>
2018-06-04 18:16:44 +0200

    adding header

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import Header from '../components/Header';
 import './App.css';
 
 import { setSearchField, requestRobots } from '../actions.js'
@@ -35,14 +36,16 @@ class App extends Component {
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    return isPending ? (
-      <h1>Loading...</h1>
-    ) : (
+    return (
       <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange} />
+        <Header />
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
-          <CardList robots={filteredRobots} />
+          { isPending ? <h1>Loading</h1> :
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          }
         </Scroll>
       </div>
     );
