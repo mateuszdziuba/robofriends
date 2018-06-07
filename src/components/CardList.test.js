2ca91f3af467c39b5bdfcf79386d4a8966724d6b
mateuszdziuba <mkdziuba@gmail.com>
2018-06-07 14:09:35 +0200

    'testing'

---
+++ src/components/CardList.test.js
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import CardList from './CardList';
+
+it('expect to render CardList component', () => {
+  const mockRobots = [
+    {
+      id: 1,
+      name: 'John Snow',
+      email: 'john@snow.com'
+    }
+  ]
+  expect(shallow(<CardList robots={mockRobots}/>)).toMatchSnapshot();
+})
